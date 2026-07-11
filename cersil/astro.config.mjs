f95f3e107f4531323464fd9de126888832daef5b
fxadilima <xaverius2017@gmail.com>
2026-07-11 13:45:26 +0700

    switching to server

--- cersil/astro.config.mjs
+++ cersil/astro.config.mjs
@@ -1,9 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import deno from '@astrojs/deno';
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
+    output: 'server',
+    adapter: deno({
+        port: 8000,
+        start: true,
+    }),
     integrations: [
         mdx()
     ]
